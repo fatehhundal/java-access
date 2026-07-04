18107981f132a45f66fb787e1e1ab9985c217fae
fatehhundal <fshundal@hotmail.com>
2026-07-04 12:11:19 +0800

    superSub

---
+++ superSub.java
@@ -0,0 +1,17 @@
+class Superclass {
+    int number = 56;
+}
+
+class Subclass extends Superclass {
+    int number = 96;
+    void printNumber() {
+        System.out.println(number);
+    }
+}
+
+public class superSub {
+    public static void main(String[] args) {
+        Subclass sub = new Subclass();
+        sub.printNumber();
+    }
+}

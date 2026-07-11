3681bae7bcf768bbf434b76801bff08838ffa0c7
fatehhundal <fshundal@hotmail.com>
2026-07-11 12:19:12 +0800

    sortArray

---
+++ sortArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class sortArray {
+    public static void main(String[] args) {
+        int[] numbers = {25, 10, 45, 5, 30};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
